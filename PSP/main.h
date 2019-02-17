7b3a2f280bdedb5ece2c0f7b17d241bc89ea1004
why2000 <867981746@qq.com>
2019-02-17 19:53:13 +0800

    enum

--- PSP/main.h
+++ PSP/main.h
@@ -8,3 +8,24 @@
 
 //includes
 #include<utils.h>
+
+//defines
+enum token_kind {
+	ERROR_TOKEN,
+	IDENT,
+	INT_CONST,
+	FLOAT_CONST,
+	CHAR_CONST,
+	INT,
+	FLOAT,
+	CHAR,
+	IF,
+	ELSE,
+	EQ,
+	ASSIGN,
+	LP,
+	RP,
+	SEMI,
+	COMMA
+};
+
--- PSP/utils.h
+++ PSP/utils.h
@@ -1,5 +1,8 @@
 #pragma once
 #include<main.h>
+//remove left spaces, return in heap
 char* lstrip(char*);
+//remove right spaces, return in heap
 char* rstrip(char*);
+//remove spaces, return in heap
 char* strip(char*);
